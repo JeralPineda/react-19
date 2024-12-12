7e9bc705c022e7ff9ed2bdcdef02eea7c78565b6
jcpmbhc <jpineda@banhcafe.hn>
2024-12-12 12:33:35 -0600

    fix: useOptimistic - Manejo de errores

--- src/actions/update-planet.action.ts
+++ src/actions/update-planet.action.ts
@@ -1,9 +1,18 @@
 import { planetsApi } from "../api/planetsApi";
 import { Planet } from "../interfaces/planet.interface";
 
+const sleep = async () => {
+  return new Promise((r) => setTimeout(r, 2000));
+};
+
 export const updatePlanetAction = async (planet: Planet) => {
   try {
+    sleep(); // Ralentizar 2 segundos
+
+    throw new Error("Error de prueba");
+
     const response = await planetsApi.patch<Planet>(`/${planet.id}`, planet);
+    console.log("🚀 update-planet.action.ts -> #7 ~", "Planeta actualizado");
 
     return response.data;
   } catch (error) {
--- src/pages/ui/PlanetList.tsx
+++ src/pages/ui/PlanetList.tsx
@@ -1,4 +1,4 @@
-import { useState, useOptimistic } from "react";
+import { useState, useOptimistic, useTransition } from "react";
 import { updatePlanetAction } from "../../actions/update-planet.action";
 import { Planet } from "../../interfaces/planet.interface";
 
@@ -7,6 +7,8 @@ interface Props {
 }
 
 export const PlanetList = ({ planets }: Props) => {
+  const [isPending, startTransition] = useTransition();
+
   const [optimisticPlanets, setOptimisticPlanets] = useOptimistic(
     planets,
     (current: Planet[], newPlanet: Planet) => {
@@ -19,11 +21,21 @@ export const PlanetList = ({ planets }: Props) => {
   );
 
   const handleUpdatePlanet = async (planet: Planet) => {
-    planet.name = planet.name.toUpperCase();
+    startTransition(async () => {
+      const data = {
+        ...planet,
+        name: planet.name.toUpperCase(),
+      };
 
-    setOptimisticPlanets(planet);
-
-    const updatedPlanet = await updatePlanetAction(planet);
+      try {
+        setOptimisticPlanets(data);
+        const updatedPlanet = await updatePlanetAction(data);
+        setOptimisticPlanets(updatedPlanet);
+      } catch (error) {
+        console.log(error);
+        setOptimisticPlanets(planet);
+      }
+    });
   };
 
   return (
@@ -36,8 +48,9 @@ export const PlanetList = ({ planets }: Props) => {
 
           <br />
           <button
-            className="bg-blue-500 text-white p-2 rounded w-full"
+            className="bg-blue-500 text-white p-2 rounded w-full disabled:bg-gray-500"
             onClick={() => handleUpdatePlanet(planet)}
+            disabled={isPending}
           >
             Actualizar
           </button>
